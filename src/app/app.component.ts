148e4bbbbd49776b07dc336d4bd44842ca6844be
Nivas Donthireddy <nivas.donthireddy@bp.com>
2022-04-26 09:47:04 +0530

    Dummy Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Post } from './Post';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    
 })
 export class AppComponent {
-  title = 'angular_test_project';
+    data: Post[] = [];
+    loading: boolean;
+    constructor(private http: HttpClient){}
+    
+    makeRequest(): void {
+        this.loading = true;
+        this.http
+        .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+        .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+        });
+    }
+
+    onChange(event: any){
+        console.log(event.target.files);
+    }
+        
 }
